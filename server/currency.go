5917bbcb4e59b5cf1f6a13e84b796a378cd65391
Sibi <sibi.kabil@gmail.com>
2020-10-03 18:47:53 +0530

    Currency conversion current rate from external api

--- server/currency.go
+++ server/currency.go
@@ -2,6 +2,10 @@ package server
 
 import (
 	"context"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
 
 	"github.com/hashicorp/go-hclog"
 	protos "github.com/sibis/currency-converter/protos/currency"
@@ -15,8 +19,24 @@ func NewCurrency(l hclog.Logger) *Currency {
 	return &Currency{l}
 }
 
-func (c *Currency) GetRate(ctx context.Context, rr *protos.RateRequest) (*protos.RateResponse, error) {
-	c.log.Info("Handle Getrate", "base", rr.GetBase(), "destination", rr.GetDestination())
+type Conversion struct {
+	Rates map[string]float32 `json:"rates"`
+	Base  string             `json:"base"`
+	Date  string             `json:"date"`
+}
 
-	return &protos.RateResponse{Rate: 0.5}, nil
+func (c *Currency) GetRate(ctx context.Context, rr *protos.RateRequest) (*protos.RateResponse, error) {
+
+	c.log.Info("Handle Generate", "base", rr.GetBase(), "destination", rr.GetDestination())
+	url := fmt.Sprintf("https://api.exchangeratesapi.io/latest?symbols=%v&base=%v", rr.GetBase(), rr.GetDestination())
+	resp, err := http.Get(url)
+	if err != nil {
+		fmt.Println("Err : ", err)
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	var currency Conversion
+	err = json.Unmarshal(body, &currency)
+	str := fmt.Sprintf("%v", rr.GetBase())
+	return &protos.RateResponse{Rate: currency.Rates[str]}, nil
 }
